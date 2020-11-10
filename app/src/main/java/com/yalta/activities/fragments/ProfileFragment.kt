f309b07d6aded3f282bce11f9aefe599ddc87a01
AnastasiiaTitova <staceylemann@gmail.com>
2020-11-10 17:03:22 +0300

    got rid of observables

--- app/src/main/java/com/yalta/activities/fragments/ProfileFragment.kt
+++ app/src/main/java/com/yalta/activities/fragments/ProfileFragment.kt
@@ -7,7 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.yalta.R
 import com.yalta.activities.LoginActivity
@@ -16,32 +15,22 @@ import com.yalta.services.SessionService
 import com.yalta.viewmodel.ProfileViewModel
 
 class ProfileFragment : Fragment() {
-    private lateinit var viewModel: ProfileViewModel
+    private val viewModel by lazy { ViewModelProvider(this).get(ProfileViewModel::class.java) }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding: FragmentProfileBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_profile, container, false)
+        val binding: FragmentProfileBinding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_profile, container, false)
 
         binding.logout.setOnClickListener {
             SessionService.discardSession()
             startActivity(Intent(activity, LoginActivity::class.java))
         }
 
-        binding.changeValue.setOnClickListener {
-            viewModel.changeValue()
-        }
-
-        viewModel = ViewModelProvider(this).get(ProfileViewModel::class.java)
+        binding.viewModel = viewModel
         binding.lifecycleOwner = this
-
-        viewModel.userName.observe(viewLifecycleOwner, Observer<String> { newName ->
-            binding.name = newName
-        })
-        viewModel.role.observe(viewLifecycleOwner, Observer<String> { newRole ->
-            binding.role = newRole
-        })
 
         return binding.root
     }
