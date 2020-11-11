d5f3022012956e7d00196225258505f9b27134a4
AnastasiiaTitova <33322723+AnastasiiaTitova@users.noreply.github.com>
2020-11-11 17:09:09 +0300

    Added data binding for ProfileFragment (#7)
    ProfileViewModel should get data via REST in future

--- app/src/main/java/com/yalta/activities/fragments/ProfileFragment.kt
+++ app/src/main/java/com/yalta/activities/fragments/ProfileFragment.kt
@@ -2,30 +2,36 @@ package com.yalta.activities.fragments
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.yalta.R
 import com.yalta.activities.LoginActivity
+import com.yalta.databinding.FragmentProfileBinding
 import com.yalta.services.SessionService
+import com.yalta.viewmodel.ProfileViewModel
 
 class ProfileFragment : Fragment() {
+    private val viewModel by lazy { ViewModelProvider(this).get(ProfileViewModel::class.java) }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_profile, container, false)
+        val binding: FragmentProfileBinding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_profile, container, false)
 
-        val button: Button = view.findViewById(R.id.logout)
-        button.setOnClickListener {
+        binding.logout.setOnClickListener {
             SessionService.discardSession()
             startActivity(Intent(activity, LoginActivity::class.java))
         }
 
-        return view
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = this
+
+        return binding.root
     }
 }
