a152bd1092b4083fc948e006088c7e2ac54972a0
AnastasiiaTitova <staceylemann@gmail.com>
2020-11-18 00:29:06 +0300

    Added Google map to Map fragment
    Now Map fragment shows map with current user location. Before all manipulations user is asked to grant permissions to track location.

--- app/src/main/java/com/yalta/activities/fragments/MapFragment.kt
+++ app/src/main/java/com/yalta/activities/fragments/MapFragment.kt
@@ -1,19 +1,150 @@
 package com.yalta.activities.fragments
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.*
+import com.google.android.gms.maps.model.LatLng
 
 import com.yalta.R
 
-class MapFragment : Fragment() {
+class MapFragment : Fragment(), OnMapReadyCallback {
+    private lateinit var mapView: MapView
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+
+    private val defaultLocation = LatLng(59.9571, 30.3082)
+    private val defaultZoom = 16.0F
+    private var lastKnownLocation: Location? = null
+    private var map: GoogleMap? = null
+    private var locationPermissionGranted = false
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_map, container, false)
+        val view = inflater.inflate(R.layout.fragment_map, container, false)
+
+        mapView = view.findViewById(R.id.map)
+        mapView.onCreate(savedInstanceState)
+        mapView.onResume()
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(requireContext())
+        mapView.getMapAsync(this)
+
+        return view
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        mapView.onPause()
+        super.onPause()
+    }
+
+    override fun onDestroy() {
+        mapView.onDestroy()
+        super.onDestroy()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
+    }
+
+    override fun onLowMemory() {
+        mapView.onLowMemory()
+        super.onLowMemory()
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+        getLocationPermission()
+        updateLocation()
+        getDeviceLocation()
+    }
+
+    private fun updateLocation() {
+        if (map == null) {
+            return
+        }
+        try {
+            if (locationPermissionGranted) {
+                map?.isMyLocationEnabled = true
+                map?.uiSettings?.isMyLocationButtonEnabled = true
+            } else {
+                map?.isMyLocationEnabled = false
+                map?.uiSettings?.isMyLocationButtonEnabled = false
+                lastKnownLocation = null
+                getLocationPermission()
+            }
+        } catch (e: SecurityException) {
+            Log.e("SecurityException", "This should not happen")
+        }
+    }
+
+    private fun getDeviceLocation() = try {
+        val locationResult = fusedLocationProviderClient.lastLocation
+        locationResult.addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                lastKnownLocation = task.result
+                if (lastKnownLocation != null) {
+                    map?.moveCamera(
+                        CameraUpdateFactory.newLatLngZoom(
+                            LatLng(
+                                lastKnownLocation!!.latitude,
+                                lastKnownLocation!!.longitude
+                            ), defaultZoom
+                        )
+                    )
+                }
+            } else {
+                map?.moveCamera(
+                    CameraUpdateFactory
+                        .newLatLngZoom(defaultLocation, defaultZoom)
+                )
+                map?.uiSettings?.isMyLocationButtonEnabled = false
+            }
+        }
+    } catch (e: SecurityException) {
+        Log.e("SecurityException", "This should not happen")
+    }
+
+    private fun getLocationPermission() {
+        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION)
+            == PackageManager.PERMISSION_GRANTED
+        ) {
+            locationPermissionGranted = true
+        } else {
+            ActivityCompat.requestPermissions(
+                requireActivity().parent, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                1
+            )
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        locationPermissionGranted = false
+        if (requestCode == 1) {
+            if (grantResults.isNotEmpty() &&
+                grantResults[0] == PackageManager.PERMISSION_GRANTED
+            ) {
+                locationPermissionGranted = true
+            }
+        }
+        updateLocation()
     }
 }
